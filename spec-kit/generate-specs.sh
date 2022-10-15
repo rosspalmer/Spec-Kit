d9c3c9c314fb10622470ea70d16bbf31794d4909
Ross Palmer <rossbpalmer@gmail.com>
2022-10-15 02:36:14 -0600

    add output write

--- spec-kit/generate-specs.sh
+++ spec-kit/generate-specs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# OUTPUT_LOCATION=$1
+OUTPUT_LOCATION=$1
 
 # Get `hostnamectl` values
 HOSTNAMECTL=$(hostnamectl)
@@ -8,27 +8,11 @@ HOSTNAME=$(echo "$HOSTNAMECTL" | grep "hostname" | awk -F ':' '{ print $2 }' | x
 ICON_NAME=$(echo "$HOSTNAMECTL" | grep "Icon name" | awk -F ':' '{ print $2 }' | xargs)
 MACHINE_ID=$(echo "$HOSTNAMECTL" | grep "Machine" | awk -F ':' '{ print $2 }' | xargs)
 
-# Pack hostnamectl values into JSON
-HOSTNAME_JSON=$(cat << EOF
-{
-  "hostname": "$HOSTNAME",
-  "icon_name": "$ICON_NAME",
-  "machine_id": "$MACHINE_ID"
-}
-EOF
-)
-
 echo "===+=== hostnamectl loaded ===+==="
 echo
-echo "$HOSTNAME_JSON"
-echo
-
-# Get `lshw` in JSON format
-LSHW_JSON=$(lshw -json)
-
-echo "===+=== lshw loaded ===+==="
-echo
-lshw -short
+echo "hostname -> $HOSTNAME"
+echo "icon_name -> $ICON_NAME"
+echo "machine_id -> $MACHINE_ID"
 echo
 
 # Get `lscpu` CPU specs in JSON format
@@ -46,3 +30,36 @@ echo "===+=== lspci loaded ===+==="
 echo
 lspci
 echo
+
+# Get `lshw` in JSON format
+LSHW_JSON=$(lshw -json)
+
+echo "===+=== lshw loaded ===+==="
+echo
+lshw -short
+echo
+
+# Get total memory using `free`
+TOTAL_MEMORY=$(free | awk '{if (NR==2) print $2}')
+
+# Pack final json
+OUTPUT_JSON=$(cat << EOF
+{
+
+  "hostname": "$HOSTNAME",
+  "icon_name": "$ICON_NAME",
+  "machine_id": "$MACHINE_ID",
+  "cpu": "TODO",
+  "total_memory": "$TOTAL_MEMORY",
+
+  "lscpu": "$LSCPU_JSON",
+
+  "lspci": "$LSPCI_JSON",
+
+  "lshw": "$lshw"
+
+}
+EOF
+)
+
+OUTPUT_JSON > "$OUTPUT_LOCATION/$MACHINE_ID.json"
