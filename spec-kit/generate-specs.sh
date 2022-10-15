2d78f7223191668546876212b9c4e00ab5566d96
Ross Palmer <rossbpalmer@gmail.com>
2022-10-15 01:59:31 -0600

    try again

--- spec-kit/generate-specs.sh
+++ spec-kit/generate-specs.sh
@@ -8,8 +8,13 @@ HOSTNAME=$(echo "$HOSTNAMECTL" | grep "hostname" | awk -F ':' '{ print $2 }' | x
 ICON_NAME=$(echo "$HOSTNAMECTL" | grep "Icon name" | awk -F ':' '{ print $2 }' | xargs)
 MACHINE_ID=$(echo "$HOSTNAMECTL" | grep "Machine" | awk -F ':' '{ print $2 }' | xargs)
 
+echo "$HOSTNAME"
+echo "$ICON_NAME"
+echo "$MACHINE_ID"
+
 # Pack hostnamectl values into JSON
-HOSTNAME_JSON=$(cat << EOF {
+HOSTNAME_JSON=$(cat << EOF
+{
   "hostname": "$HOSTNAME"
 }
 EOF
