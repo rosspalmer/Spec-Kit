16468e00b15031c9ad77e8959bd84ce4b58b4f5e
Ross Palmer <rossbpalmer@gmail.com>
2022-10-14 22:24:35 -0600

    simpler hostname json script

--- spec-kit/generate-specs.sh
+++ spec-kit/generate-specs.sh
@@ -4,28 +4,11 @@ OUTPUT_LOCATION=$1
 
 HOSTNAMECTL=$(hostnamectl)
 
-# Matches lines from `hostnamectl` output
-hostnamectl_value () {
-  KEY=$1
-  VAL=$("$HOSTNAMECTL" | grep "$KEY")
-  [[ $VAL =~ ^.+:\s(.+)$ ]] && \
-    echo BASH_REMATCH[1]
-}
-
 # Use machine ID from hostnamectl as unique identifier
-UID=$(hostnamectl_value 'Machine ID')
+UID=$(grep "Machine" | awk -F':' '{ print $2 }')
 
 # Pack hostnamectl values into JSON
-HOSTNAME_JSON=$(cat << EOF
-{
-  "hostname": "$(hostnamectl_value 'hostname')",
-  "icon_name": "$(hostnamectl_value 'Icon Name')",
-  "chassis": "$(hostnamectl_value 'Chassis')",
-  "machine_id": "$(hostnamectl_value 'Machine ID')",
-  "operating_system": "$(hostnamectl_value 'Operating System')"
-}
-EOF
-)
+HOSTNAME_JSON="{$(echo HOSTNAMECTL | awk -F':' '{ print "$1":"$2", }')}"
 
 echo "$HOSTNAME_JSON"
 
