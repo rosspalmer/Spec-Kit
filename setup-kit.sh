19f9ae1d5b191f6874549c3b5c1043b86ecf7ebf
Ross Palmer <rossbpalmer@gmail.com>
2022-10-17 17:58:07 -0600

    fix high level su -c commands

--- setup-kit.sh
+++ setup-kit.sh
@@ -6,6 +6,6 @@ ROOT_PATH="$(dirname -- "$(realpath "${BASH_SOURCE:-$0}")")"
 # FIXME remove debug print
 echo "ROOT PATH: $ROOT_PATH"
 
-su -c "sh ""$ROOT_PATH"/spec-kit/required-kit.sh""
+su -c "sh $ROOT_PATH/spec-kit/required-kit.sh"
 
 echo "alias spec-kit=\"sh $ROOT_PATH/spec-kit.sh\"" >> ~/.bashrc
--- spec-kit.sh
+++ spec-kit.sh
@@ -6,4 +6,4 @@ ROOT_PATH="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
 # Make spec-data folder in HOME directory if not exists
 mkdir -p ~/spec-data
 
-sh "$ROOT_PATH/spec-kit/generate-specs.sh" ~/spec-data
+su -c "sh $ROOT_PATH/spec-kit/generate-specs.sh ~/spec-data"
