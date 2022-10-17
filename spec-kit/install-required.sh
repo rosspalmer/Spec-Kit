2dcf79e4b2879d465f25dad5d291da3e87e726db
Ross Palmer <rossbpalmer@gmail.com>
2022-10-16 20:16:35 -0600

    add pathing options for install-required.sh

--- spec-kit/install-required.sh
+++ spec-kit/install-required.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# Install required packages
-apt-get install lshw
-apt-get install git
-apt-get install squashfs-tools
-apt-get install genisoimage
+RUN_MODE=$1
+
+if [ "$RUN_MODE" == 'spec-only' ] || [ "$RUN_MODE" == 'linux-live' ]; then
+
+  # Install required packages for spec job
+  apt-get install lshw
+
+  # Install required packages
+  if [ "$RUN_MODE" == 'linux-live' ]; then
+    apt-get install git
+    apt-get install squashfs-tools
+    apt-get install genisoimage
+  fi
+
+else
+
+  echo "Incorrect run-mode argument $RUN_MODE: Use spec-only or linux-live"
+
+fi
