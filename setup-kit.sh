d9065709b249876112a15a35539e99b4692d6cd4
Ross Palmer <rossbpalmer@gmail.com>
2022-10-17 17:56:18 -0600

    fix issues with setup script

--- setup-kit.sh
+++ setup-kit.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Get root path for spec-ket package
-ROOT_PATH="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
+ROOT_PATH="$(dirname -- "$(realpath "${BASH_SOURCE:-$0}")")"
 
-sh "$ROOT_PATH/spec-kit/required-kit.sh"
+# FIXME remove debug print
+echo "ROOT PATH: $ROOT_PATH"
+
+su -c "sh ""$ROOT_PATH"/spec-kit/required-kit.sh""
 
 echo "alias spec-kit=\"sh $ROOT_PATH/spec-kit.sh\"" >> ~/.bashrc
-
-
