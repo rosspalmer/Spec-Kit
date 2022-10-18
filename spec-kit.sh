8a2e1fca5b8cd9dd4660677fd79a9f085a9ab63b
Ross Palmer <rossbpalmer@gmail.com>
2022-10-17 18:35:33 -0600

    use $HOME variable

--- spec-kit.sh
+++ spec-kit.sh
@@ -8,4 +8,4 @@ echo "FIXME: ROOT $ROOT_PATH"
 # Make spec-data folder in HOME directory if not exists
 mkdir -p ~/spec-data
 
-su -c "sh $ROOT_PATH/spec-kit/generate-specs.sh ~/spec-data"
+su -c "sh $ROOT_PATH/spec-kit/generate-specs.sh $HOME/spec-data"
