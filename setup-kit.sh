65a04ca98a67c0e715784d2b4342a1c7866a9336
Ross Palmer <rossbpalmer@gmail.com>
2022-10-17 18:30:55 -0600

    add pathing debug to spec kit app

--- setup-kit.sh
+++ setup-kit.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Get root path for spec-ket package
+# Get root path for spec-kit package
 ROOT_PATH="$(dirname -- "$(realpath "${BASH_SOURCE:-$0}")")"
-
-# FIXME remove debug print
-echo "ROOT PATH: $ROOT_PATH"
 
 su -c "sh $ROOT_PATH/spec-kit/required-kit.sh"
 
--- spec-kit.sh
+++ spec-kit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Get root path for spec-ket package
-ROOT_PATH="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
+ROOT_PATH="$(dirname -- "$(realpath "${BASH_SOURCE:-$0}")")"
+
+echo "FIXME: ROOT $ROOT_PATH"
 
 # Make spec-data folder in HOME directory if not exists
 mkdir -p ~/spec-data
