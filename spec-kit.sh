4d93579351d11a1ceb9c651aadee40611e213fe4
Ross Palmer <rossbpalmer@gmail.com>
2022-10-17 18:38:00 -0600

    remove debug print

--- spec-kit.sh
+++ spec-kit.sh
@@ -3,8 +3,6 @@
 # Get root path for spec-ket package
 ROOT_PATH="$(dirname -- "$(realpath "${BASH_SOURCE:-$0}")")"
 
-echo "FIXME: ROOT $ROOT_PATH"
-
 # Make spec-data folder in HOME directory if not exists
 mkdir -p ~/spec-data
 
