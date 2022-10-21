a02a6f509ba9127c95acb1091d891485c2917391
Ross Palmer <rossbpalmer@gmail.com>
2022-10-21 10:12:00 -0600

    use sudo instead of su -c

--- setup-kit.sh
+++ setup-kit.sh
@@ -3,6 +3,6 @@
 # Get root path for spec-kit package
 ROOT_PATH="$(dirname -- "$(realpath "${BASH_SOURCE:-$0}")")"
 
-su -c "sh $ROOT_PATH/spec-kit/required-kit.sh"
+sudo sh $ROOT_PATH/spec-kit/required-kit.sh
 
 echo "alias spec-kit=\"sh $ROOT_PATH/spec-kit.sh\"" >> ~/.bashrc
