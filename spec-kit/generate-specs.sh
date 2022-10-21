dafa2f05b5c307d8f6f2726af88172c445045415
Ross Palmer <rossbpalmer@gmail.com>
2022-10-21 10:11:14 -0600

    add sudo to lshw

--- spec-kit/generate-specs.sh
+++ spec-kit/generate-specs.sh
@@ -23,7 +23,7 @@ echo "===+=== lscpu loaded ===+==="
 
 # Get `lshw` in JSON format
 # FIXME remove outer list
-LSHW_STRING=$(lshw -json)
+LSHW_STRING=$(sudo lshw -json)
 LSHW_END=$(expr ${#LSHW_STRING} - 1)
 LSHW_JSON=$(echo "$LSCPU_STRING" | cut -c2-"$LSHW_END")
 
