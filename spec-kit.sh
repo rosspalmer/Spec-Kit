c7511c1fbbb916f1625c048f9602f447db8a91b5
Ross Palmer <rossbpalmer@gmail.com>
2022-10-21 10:12:39 -0600

    remove super user call for calling script

--- spec-kit.sh
+++ spec-kit.sh
@@ -6,4 +6,4 @@ ROOT_PATH="$(dirname -- "$(realpath "${BASH_SOURCE:-$0}")")"
 # Make spec-data folder in HOME directory if not exists
 mkdir -p ~/spec-data
 
-su -c "sh $ROOT_PATH/spec-kit/generate-specs.sh $HOME/spec-data"
+sh $ROOT_PATH/spec-kit/generate-specs.sh $HOME/spec-data
