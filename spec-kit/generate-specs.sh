c7e977a7518e6f6c8e28f7c857f3cb806bc9eba6
Ross Palmer <rossbpalmer@gmail.com>
2023-05-25 22:26:25 -0600

    Update generate-specs.sh

--- spec-kit/generate-specs.sh
+++ spec-kit/generate-specs.sh
@@ -6,12 +6,12 @@ OUTPUT_LOCATION=$2
 HARDWARE_FOLDER="${OUTPUT_LOCATION}/${HARDWARE_KEY}"
 
 mkdir $HARDWARE_FOLDER
-lscpu --json > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_lscpu.json"
-lsblk --json > "${HARDWARE_KEY}_lsblk.json"
-lspci --vm > "${HARDWARE_KEY}_lspci.txt"
-dmidecode -t memory > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_dmidecode_memory.txt"
-dmidecode -t bios > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_dmidecode_bios.txt"
-dmidecode -t system > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_dmidecode_system.txt"
+sudo lscpu --json > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_lscpu.json"
+sudo lsblk --json > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_lsblk.json"
+sudo lspci -vm > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_lspci.txt"
+sudo dmidecode -t memory > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_dmidecode_memory.txt"
+sudo dmidecode -t bios > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_dmidecode_bios.txt"
+sudo dmidecode -t system > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_dmidecode_system.txt"
 
-zip -r "${HARDWARE_FOLDER}.zip" $HARWARE_FOLDER
+zip -r "${HARDWARE_FOLDER}.zip" "$HARWARE_FOLDER"
 
