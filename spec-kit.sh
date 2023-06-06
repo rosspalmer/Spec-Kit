eb86a11b816ea26fe06fe36e6b86ba7ef5855433
Ross Palmer <rossbpalmer@gmail.com>
2023-06-06 00:57:35 -0400

    removed old excess

--- spec-kit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get root path for spec-ket package
-ROOT_PATH="$(dirname -- "$(realpath "${BASH_SOURCE:-$0}")")"
-
-# Make spec-data folder in HOME directory if not exists
-mkdir -p ~/spec-data
-
-sh $ROOT_PATH/spec-kit/generate-specs.sh $HOME/spec-data
