1644e6cec9bd43acd6c413016a238508dbb4df3f
Ross Palmer <rossbpalmer@gmail.com>
2023-06-06 16:50:09 -0600

    Update setup-kit.sh

--- setup-kit.sh
+++ setup-kit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# Get shared output location to be stored in the 
+OUTPUT_PATH=$1
+
 # Get root path for spec-kit package
 ROOT_PATH="$(dirname -- "$(realpath "${BASH_SOURCE:-$0}")")"
 
-sudo sh $ROOT_PATH/spec-kit/required-kit.sh
-
-echo "alias spec-kit=\"sh $ROOT_PATH/spec-kit.sh\"" >> ~/.bashrc
+echo 'alias spec-kit="sudo sh $ROOT_PATH/spec-kit.sh $OUTPUT_PATH"' >> ~/.bashrc
