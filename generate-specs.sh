057fa540eaefa571f71a09d6fefbec74d73f3c12
Ross Palmer <rossbpalmer@gmail.com>
2023-06-09 08:25:52 -0600

    move into folder

--- generate-specs.sh
+++ generate-specs.sh
@@ -9,18 +9,20 @@ HARDWARE_FOLDER="${OUTPUT_LOCATION}/${HARDWARE_KEY}"
 
 # Generate unique folder at OUTPUT_LOCATION using input HARDWARE_KEY parameter
 mkdir $HARDWARE_FOLDER
+cd $HARDWARE_FOLDER
 
 # Run hardware spec utilities and either save as JSON or raw text formats
-sudo lscpu --json > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_lscpu.json"
-sudo lsblk --json > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_lsblk.json"
-sudo lspci -vm > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_lspci.txt"
-sudo dmidecode -t memory > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_dmidecode_memory.txt"
-sudo dmidecode -t bios > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_dmidecode_bios.txt"
-sudo dmidecode -t system > "${HARDWARE_FOLDER}/${HARDWARE_KEY}_dmidecode_system.txt"
+sudo lscpu --json > "${HARDWARE_KEY}_lscpu.json"
+sudo lsblk --json > "${HARDWARE_KEY}_lsblk.json"
+sudo lspci -vm > "${HARDWARE_KEY}_lspci.txt"
+sudo dmidecode -t memory > "${HARDWARE_KEY}_dmidecode_memory.txt"
+sudo dmidecode -t bios > "${HARDWARE_KEY}_dmidecode_bios.txt"
+sudo dmidecode -t system > "${HARDWARE_KEY}_dmidecode_system.txt"
 
 # Zip all data files in generated folder into single 
 # compressed file with HARDWARE_KEY input as file name 
-zip -r $HARDWARE_FOLDER.zip $HARDWARE_FOLDER/
+cd ..
+zip -r $HARDWARE_KEY.zip $HARDWARE_KEY/
 
 # Remove folder of now zipped data
-rm -r $HARDWARE_FOLDER
+rm -r $HARDWARE_KEY
