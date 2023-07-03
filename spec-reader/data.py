1840369c66668ac3ac5747ed23f89224c75d6469
Ross Palmer <rossbpalmer@gmail.com>
2023-07-03 14:25:53 -0600

    add unit id to data classes

--- spec-reader/data.py
+++ spec-reader/data.py
@@ -4,8 +4,9 @@ from typing import List
 
 class CPU:
 
-    def __init__(self, model_name: str, total_cpu_count: str, cores_per_socket: str,
+    def __init__(self, unit_id: str, model_name: str, total_cpu_count: str, cores_per_socket: str,
                  sockets: str, max_mhz: float, min_mhz: float):
+        self.unit_id = unit_id
         self.model_name = model_name
         self.total_cpu_count = total_cpu_count
         self.cores_per_socket = cores_per_socket
@@ -13,9 +14,10 @@ class CPU:
         self.max_mhz = max_mhz
         self.min_mhz = min_mhz
 
+
 class RAM:
 
-    def __init__(self, handle: str, size: str, speed: str, form_factor: str, ram_type: str):
+    def __init__(self, unit_id: str, handle: str, size: str, speed: str, form_factor: str, ram_type: str):
         self.handle = handle
         self.size = size
         self.speed = speed
@@ -25,7 +27,7 @@ class RAM:
 
 class HardwareSpec:
 
-    def __init__(self, uid: str, cpu: CPU, ram: List[RAM]):
-        self.uid = uid
+    def __init__(self, unit_id: str, cpu: CPU, ram: List[RAM]):
+        self.unit_id = unit_id
         self.cpu = cpu
         self.ram = ram
