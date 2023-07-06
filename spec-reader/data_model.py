71b36b342a8919b5db167a6fad4c6a4fffd07ecd
Ross Palmer <rossbpalmer@gmail.com>
2023-07-06 16:48:11 -0600

    add part tables and id

--- spec-reader/data_model.py
+++ spec-reader/data_model.py
@@ -16,12 +16,21 @@ class CPU(SqlBase):
     __tablename__ = "cpu"
 
     unit_id: Mapped[str] = mapped_column(primary_key=True)
+    part_id: Mapped[int]
     model_name: Mapped[str]
     total_cpu_count: Mapped[int]
     cores_per_socket: Mapped[int]
     sockets: Mapped[int]
     max_mhz: Mapped[float]
     min_mhz: Mapped[float]
+
+
+class CPUParts(SqlBase):
+
+    __tablename__ = "cpu_parts"
+
+    part_id: Mapped[int] = mapped_column(autoincrement=True)
+    model_name: Mapped[str]
 
 
 class RAM(SqlBase):
@@ -31,7 +40,16 @@ class RAM(SqlBase):
     unit_id: Mapped[str] = mapped_column(primary_key=True)
     array_handle: Mapped[str] = mapped_column(primary_key=True)
     ram_handle: Mapped[str] = mapped_column(primary_key=True)
+    part_id: Mapped[int]
+    manufacturer: Mapped[str]
+    part_number: Mapped[str]
     size_mb: Mapped[int]
     speed_mts: Mapped[int]
     form_factor: Mapped[str]
     ram_type: Mapped[str]
+
+
+class RAMPart(SqlBase):
+
+    __tablename__ = "ram_parts"
+
